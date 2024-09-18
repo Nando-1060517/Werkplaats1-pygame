2de0192acba6e206ed2733e0fbc1a4323502092c
Elmar-1112449 <1112449@hr.nl>
2024-09-18 14:35:17 +0200

    Update CoreSurvival.py
    Cleaned up part of the script including:
    - Variables
    - Unnecessary code
    
    Added:
    - Enemy rotate when in range of player

--- CoreSurvival.py
+++ CoreSurvival.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 import pygame_gui
 import random
@@ -11,16 +12,18 @@ display_info = pygame.display.Info()
 screen = pygame.display.set_mode(pygame.Vector2(display_info.current_w, display_info.current_h))
 clock = pygame.time.Clock()
 
+player_coins = 0
+player_wood = 0
+player_stone = 0
+
+items_list = []
+
 #pos
 center = pygame.Vector2(display_info.current_w / 2, display_info.current_h / 2)
 random_range_x = 700
 random_range_y = 350
 
 #UI
-player_coins = 0
-player_wood = 0
-player_stone = 0
-
 UI_Manager = pygame_gui.UIManager((display_info.current_w, display_info.current_h))
 UI_items = pygame_gui.elements.UITextBox('', pygame.Rect(0, 0, 300, 100), UI_Manager)
 UI_pos = pygame_gui.elements.UITextBox('', pygame.Rect(300, 0, 300, 100), UI_Manager)
@@ -66,23 +69,14 @@ class Player:
         self.move.y = (keys[pygame.K_s] - keys[pygame.K_w])
 
     def collision(self):
-        for item in items_list[:]:
-            collide = self.rect.colliderect(item)
-            if collide:
-                print(f'collision: {collide}')
+        for item in items_list:
+            collided = self.rect.colliderect(item)
+            if collided:
                 items_list.remove(item)
 
     def draw(self):
-        pygame.draw.rect(screen, 'red', self.rect, 5)
-        screen.blit(self.image, self.rect)
-
-    def shoot(self):
-        #get player pos
-        #instantiate bullet
-        #move bullet towards current mouse pos
-        #if hits object damage it if has health
-        #delete bullet oncollision or dist passed
-        screen.blit(bullet_img, self.rect)
+        pygame.draw.rect(screen, 'red', self.rect, 5) #border
+        screen.blit(self.image, self.rect) #img
 
     def manager(self):
         self.handle_input()
@@ -96,73 +90,67 @@ class Item:
         self.pos = random_pos()
         self.coin_img = coin_img
         self.rect = pygame.Rect(self.pos.x, self.pos.y, self.coin_img.get_width(), self.coin_img.get_height())
-        self.radius = 10
-        self.size = 20
-
-
-    def draw(self, layer):
-        raise NotImplementedError("woops")
 
 class Coins(Item):
-    def draw(self, layer):
+    def draw(self):
         pygame.draw.rect(screen, 'red', self.rect, 5)
         screen.blit(self.coin_img, self.pos)
 
 class Wood(Item):
-    def draw(self, layer):
+    def draw(self):
         pygame.draw.rect(screen, 'red', self.rect, 5)
         screen.blit(self.coin_img, self.pos)
 
 class Stone(Item):
-    def draw(self, layer):
+    def draw(self):
         pygame.draw.rect(screen, 'red', self.rect, 5)
         screen.blit(self.coin_img, self.pos)
 
-items_list = []
-
-for _ in range(5):
+for _ in range(10):
     items_list.append(Stone())
     items_list.append(Wood())
     items_list.append(Coins())
 
-def draw_items(layer):
+def draw_items():
     for item in items_list:
-        item.draw(layer)
+        item.draw()
 #endregion
 
 class Enemy:
     def __init__(self):
         #Gets image and scales it down and add rot
-        self.image = enemy_img
+        self.original_image = enemy_img
+        self.image = self.original_image
+        self.image = pygame.transform.rotate(self.image, 0)
 
         #Get pos
         self.current_pos = random_pos()
-        self.pos = pygame.Rect(self.current_pos.x, self.current_pos.y, 1, 1)
+        self.rect = pygame.Rect(self.current_pos.x, self.current_pos.y, 1, 1)
 
         #enemy base stats
         self.enemy_health = 100
 
     def draw(self):
         #Change this so instead of check player pos compared to enemy, rotate enemy ONLY when chasing
-        if player.rect.x < self.pos.x:
-            if player.rect.y < self.pos.y:
-                screen.blit(pygame.transform.flip(self.image, True, False), self.pos)
-            else:
-                screen.blit(pygame.transform.flip(self.image, True, True), self.pos)
-        else:
-            if player.rect.y < self.pos.y:
-                screen.blit(pygame.transform.flip(self.image, False, False), self.pos)
-            else:
-                screen.blit(pygame.transform.flip(self.image, False, True), self.pos)
+        screen.blit(self.image, self.rect)
 
-    def move_towards(self):
-        target_pos = pygame.math.Vector2(player.rect.x, player.rect.y)
-        current_pos = pygame.math.Vector2(self.pos.x, self.pos.y)
+    def rot_enemy(self, target_pos):
+        #get player pos and change to angle
+        direction = pygame.Vector2(target_pos) - pygame.Vector2(self.rect.center)
+        angle = math.degrees(math.atan2(direction.y, direction.x))
+
+        #convert img rot to face towards player with offset since wrong front plane
+        offset = -90
+        self.image = pygame.transform.rotate(self.original_image, -angle + offset)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def move_towards(self, target_pos):
+        current_pos = pygame.math.Vector2(self.rect.x, self.rect.y)
         distance = current_pos.distance_to(target_pos)
         max_dist = 250
         min_dist = 50
-
         direction = target_pos - current_pos
+
         try:
             direction.normalize_ip()
         except ValueError:
@@ -171,21 +159,23 @@ class Enemy:
         if distance <= max_dist:
             if distance <= min_dist:
                 return
-            self.pos.topleft += direction * 5
+            else:
+                self.rot_enemy(target_pos)
+                self.rect.topleft += direction * 5
 
-    def manager(self):
-        ...
+    def manager(self, target_pos):
+        self.draw()
+        self.move_towards(target_pos)
+#endregion
 
 #region --- initialization ---
 #region initializing enemies
 enemy_amount = 10
 enemy_list = [Enemy() for enemies in range(enemy_amount)]
-enemy_list_flipped = [Enemy() for enemies in range(enemy_amount)]
 #endregion
 
 #region initializing player
 player = Player()
-#endregion
 #endregion
 #endregion
 
@@ -198,37 +188,30 @@ while running:
             running = False
         elif event.type == TIMER_EVENT:
             if frame == 3:
-                f'frame: {frame}'
                 frame = 0
             else: frame += 1
-            print(f'frame: {frame}')
     screen.fill('green')
     #endregion
 
-    #calling player
+    #region Objects
     player.manager()
+    draw_items()
 
-    #region instantiation
     for enemies in enemy_list:
-        Enemy.draw(enemies)
-        Enemy.move_towards(enemies)
-
-    draw_items(screen)
-
-    #loop through items and check for collision with player
-    #Mostly works but gotta fix the problem where it only works
-    # with rect types so not circle or ellipse
-
-
+        Enemy.manager(enemies, player.pos)
     #endregion
 
+    #region UI
     UI_items.set_text(f'Coins: {player_coins}\nWood: {player_wood}\nStone: {player_stone}')
     UI_pos.set_text(f'posX: {player.pos.x}\nPosY: {player.pos.y}')
     UI_Manager.update(clock.tick(60)/1000.0)
     UI_Manager.draw_ui(screen)
+    #endregion
 
+    #region Refresh screen and set fps
     pygame.display.flip()
     clock.tick(60)
+    #endregion
 #endregion
 
 #Quit if main loop breaks
