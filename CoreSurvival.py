50623acdcca413ad981f0ab4000188b6508f72ca
Elmar-1112449 <1112449@hr.nl>
2024-09-18 13:30:22 +0200

    Update CoreSurvival.py
    Cleaned up part of the script including:
    - Variables
    - Unnecessary code

--- CoreSurvival.py
+++ CoreSurvival.py
@@ -1,32 +1,22 @@
-#Asset pack by Cup Nooble, https://cupnooble.itch.io/
-#region To do
-#player should be able to pick up items
-#player should be able to shoot enemy
-#player gets extra coins for killing enemies
-#add animation
-
-#endregion
-
 import pygame
 import pygame_gui
 import random
 
 pygame.init()
 
-#variables
+#region variables
+#main loop
 running = True
 display_info = pygame.display.Info()
 screen = pygame.display.set_mode(pygame.Vector2(display_info.current_w, display_info.current_h))
 clock = pygame.time.Clock()
 
+#pos
 center = pygame.Vector2(display_info.current_w / 2, display_info.current_h / 2)
-
 random_range_x = 700
 random_range_y = 350
 
-player_radius = 20
-player_speed = 10
-
+#UI
 player_coins = 0
 player_wood = 0
 player_stone = 0
@@ -35,53 +25,45 @@ UI_Manager = pygame_gui.UIManager((display_info.current_w, display_info.current_
 UI_items = pygame_gui.elements.UITextBox('', pygame.Rect(0, 0, 300, 100), UI_Manager)
 UI_pos = pygame_gui.elements.UITextBox('', pygame.Rect(300, 0, 300, 100), UI_Manager)
 
-TILE_SIZE = 64
-
 TIMER_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(TIMER_EVENT, 500)
 frame = 0
 
-#Image import
+#region Image import
 player_img = pygame.image.load('Sprites/Player/player_stat.png').convert_alpha()
 enemy_img = pygame.image.load('Sprites/Enemy/enemy_stat.png').convert_alpha()
 #haven't made a bullet img yet
 bullet_img = pygame.image.load('Sprites/Items/coin.png')
 coin_img = pygame.image.load('Sprites/Items/coin.png').convert_alpha()
+#endregion
+#endregion
 
-#classes and functions
+#region Classes and functions
 def random_pos():
     return pygame.Vector2(center.x + random.randint(-random_range_x, random_range_x), center.y + random.randint(-random_range_y, random_range_y))
 
-#Followed this tutorial to make a function that can convert spritesheets into single sprites
-#https://www.youtube.com/watch?v=M6e3_8LHc7A&ab_channel=CodingWithRuss
-def get_image(sheet, width, height, scale, colour):
-    image = pygame.Surface((width, height)).convert_alpha()
-    image.blit(sheet, (0, 0), ((frame * width), 0, width, height))
-    image = pygame.transform.scale(image, (width * scale, height * scale))
-    image.set_colorkey(colour)
-    return image
-
 class Player:
     def __init__(self):
+        #import
         self.image = player_img
-        self.move_x = 0
-        self.move_y = 0
-        self.pos_x = center.x
-        self.pos_y = center.y
-        self.img_rect =self.image.get_rect()
-        self.rect = pygame.Rect(self.pos_x, self.pos_y, self.img_rect.width, self.img_rect.height)
 
-    def move(self):
-        self.pos_x += self.move_x * player_speed
-        self.pos_y += self.move_y * player_speed
-        self.rect.center = (self.pos_x, self.pos_y)
+        #base variables
+        self.speed = 10
+
+        #pos
+        self.move = pygame.Vector2(0, 0)
+        self.pos = pygame.Vector2(center.x, center.y)
+        self.rect = pygame.Rect(self.pos.x, self.pos.y, self.image.width, self.image.height)
+
+    def move_pos(self):
+        self.pos.x += self.move.x * self.speed
+        self.pos.y += self.move.y * self.speed
+        self.rect.center = (self.pos.x, self.pos.y)
 
     def handle_input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            self.shoot()
-        self.move_x = (keys[pygame.K_d] - keys[pygame.K_a])
-        self.move_y = (keys[pygame.K_s] - keys[pygame.K_w])
+        self.move.x = (keys[pygame.K_d] - keys[pygame.K_a])
+        self.move.y = (keys[pygame.K_s] - keys[pygame.K_w])
 
     def collision(self):
         for item in items_list[:]:
@@ -104,7 +86,7 @@ class Player:
 
     def manager(self):
         self.handle_input()
-        self.move()
+        self.move_pos()
         self.draw()
         self.collision()
 
@@ -149,10 +131,6 @@ def draw_items(layer):
 #endregion
 
 class Enemy:
-    move_x = 2
-    move_y = 2
-    los = 50
-
     def __init__(self):
         #Gets image and scales it down and add rot
         self.image = enemy_img
@@ -165,10 +143,17 @@ class Enemy:
         self.enemy_health = 100
 
     def draw(self):
+        #Change this so instead of check player pos compared to enemy, rotate enemy ONLY when chasing
         if player.rect.x < self.pos.x:
-            screen.blit(pygame.transform.flip(self.image, True, False), self.pos)
+            if player.rect.y < self.pos.y:
+                screen.blit(pygame.transform.flip(self.image, True, False), self.pos)
+            else:
+                screen.blit(pygame.transform.flip(self.image, True, True), self.pos)
         else:
-            screen.blit(self.image, self.pos)
+            if player.rect.y < self.pos.y:
+                screen.blit(pygame.transform.flip(self.image, False, False), self.pos)
+            else:
+                screen.blit(pygame.transform.flip(self.image, False, True), self.pos)
 
     def move_towards(self):
         target_pos = pygame.math.Vector2(player.rect.x, player.rect.y)
@@ -202,8 +187,9 @@ enemy_list_flipped = [Enemy() for enemies in range(enemy_amount)]
 player = Player()
 #endregion
 #endregion
+#endregion
 
-#main loop
+#region Main loop
 while running:
     #region event loop
     #check for exit and refresh screen by filling with background
@@ -237,11 +223,13 @@ while running:
     #endregion
 
     UI_items.set_text(f'Coins: {player_coins}\nWood: {player_wood}\nStone: {player_stone}')
-    UI_pos.set_text(f'posX: {player.pos_x}\nPosY: {player.pos_y}')
+    UI_pos.set_text(f'posX: {player.pos.x}\nPosY: {player.pos.y}')
     UI_Manager.update(clock.tick(60)/1000.0)
     UI_Manager.draw_ui(screen)
 
     pygame.display.flip()
     clock.tick(60)
+#endregion
 
+#Quit if main loop breaks
 pygame.quit()
